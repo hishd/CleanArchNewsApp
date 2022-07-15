1c20bcd34dad0c8a5c8fcf41097707fba2f822fe
Hishara Dilshan <hish2k15@gmail.com>
2022-07-15 19:24:40 +0530

    Added Resources sealed class to monitor status of network response status. Added News Repository interface functions.

--- app/src/main/java/com/hishd/cleanarchnewsapp/domain/repository/NewsRepository.kt
+++ app/src/main/java/com/hishd/cleanarchnewsapp/domain/repository/NewsRepository.kt
@@ -1,4 +1,17 @@
 package com.hishd.cleanarchnewsapp.domain.repository
 
+import com.hishd.cleanarchnewsapp.data.model.APIResponse
+import com.hishd.cleanarchnewsapp.data.model.Article
+import com.hishd.cleanarchnewsapp.data.util.Resource
+import kotlinx.coroutines.flow.Flow
+
 interface NewsRepository {
+    //Functions related to the network related operations
+    suspend fun getNewsHeadlines(): Resource<APIResponse>
+    suspend fun getSearchedNews(searchQuery: String): Resource<APIResponse>
+
+    //Functions related to the Local data related operations
+    suspend fun saveNews(article: Article)
+    suspend fun deleteNews(article: Article)
+    fun getSavedNews(): Flow<List<Article>>
 }
