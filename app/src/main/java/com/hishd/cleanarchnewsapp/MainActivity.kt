3d5678dc180de323b5e490f2beb06c091b620b2f
Hishara Dilshan <hish2k15@gmail.com>
2022-07-16 01:11:32 +0530

    Added bottom menu and connected with host fragment

--- app/src/main/java/com/hishd/cleanarchnewsapp/MainActivity.kt
+++ app/src/main/java/com/hishd/cleanarchnewsapp/MainActivity.kt
@@ -2,6 +2,8 @@ package com.hishd.cleanarchnewsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.hishd.cleanarchnewsapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +14,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setupNavigationController()
+    }
+
+    private fun setupNavigationController() {
+        val navHost = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val controller = navHost.navController
+
+        binding.containerMenu.setupWithNavController(controller)
     }
 }
